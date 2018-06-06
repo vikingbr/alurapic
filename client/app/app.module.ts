b2e51a0d66c500666c98e1ae6728c72262a164d4
vikingbr <lucasraphael.ti@gmail.com>
2018-06-05 23:37:58 -0300

    Adds get fotos in api

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
+import { FotoModule } from './foto/foto.module';
+import { HttpModule } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, FotoModule, HttpModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
